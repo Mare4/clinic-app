609a6c3a66e641f4518429e04b569d80d965f4a5
Mare4 <yuriy.rybalchenko@gmail.com>
2023-05-31 17:15:30 +0300

    feat: added adaptive for mobile device

--- src/components/MainBlock/MainBlock.js
+++ src/components/MainBlock/MainBlock.js
@@ -5,10 +5,18 @@ import Button from "../ui/Button/Button";
 export default function MainBlock() {
   return (
     <div className="main-block">
-      <h3>Здесь люди заботятся о людях</h3>
-      <h1>
-        Клиника семейного <br /> здоровья
-      </h1>
+      <div className="main-block-desktop">
+        <h3 className="main-text">Здесь люди заботятся о людях</h3>
+        <h1>
+          Клиника семейного <br /> здоровья
+        </h1>
+      </div>
+      <div className="main-block-mobile">
+        <h1 className="main-mobile-header">
+          Клиника репродуктивного здоровья и родов
+        </h1>
+        <h3 className="main-mobile-text">Место, где рождается счастье </h3>
+      </div>
       <Button className="btn center" label="Свяжитесь с нами" />
     </div>
   );
