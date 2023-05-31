2c91359b00fc161f9b850aa81113f189b90fe65e
Mare4 <yuriy.rybalchenko@gmail.com>
2023-05-31 17:25:41 +0300

    fix: fixed image

--- src/components/DoctorBlock/DoctorBlock.js
+++ src/components/DoctorBlock/DoctorBlock.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./DoctorBlock.css";
 import main_doctor from "../../assets/main_doctor.png";
-import little_shadow from "../../assets/doctor-background-shadow-little.png";
-import big_shadow from "../../assets/doctor-background-shadow-big.png";
+import shadow_for_photo from "../../assets/doctor-background-shadow.png";
 import Button from "../ui/Button/Button";
 
 export default function DoctorBlock() {
   return (
     <div className="doctor-block">
       <div>
-        <p className="doctor-name">Кузнецова Наталья Борисовна</p>
+        <p className="doctor-name">
+          Кузнецова Наталья{" "}
+          <span className="doctor-name-hidden-for-mobile">Борисовна</span>
+        </p>
         <p className="doctor-position">Главный врач клиники-роддома</p>
         <h3 className="doctor-tagline">
           Вдохновляем на здоровье, <br />а не ограничиваем запретами
@@ -19,8 +21,11 @@ export default function DoctorBlock() {
           label="Все специалисты"
         />
       </div>
-      <img className="little-shadow" src={little_shadow} alt="little-shadow" />
-      <img className="big-shadow" src={big_shadow} alt="big-shadow" />
+      <img
+        className="shadow_for_photo"
+        src={shadow_for_photo}
+        alt="shadow_for_photo"
+      />
       <img
         className="doctor-main-picture"
         src={main_doctor}
