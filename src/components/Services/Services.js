b04df12c0f4e85999d923e50de864051638b20a8
Mare4 <yuriy.rybalchenko@gmail.com>
2023-05-31 16:42:46 +0300

    feat: added adaptive for mobile device

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./Services.css";
 import services_image from "../../assets/services-image.png";
-import left_button from "../../assets/button-left.svg";
-import right_button from "../../assets/button-right.svg";
+import Dots from "../ui/Dots/Dots";
+import LeftArrow from "../ui/LeftArrow/LeftArrow";
+import RightArrow from "../ui/RightArrow/RightArrow";
 
 export default function Services() {
   return (
@@ -13,23 +14,25 @@ export default function Services() {
           src={services_image}
           alt="services_image"
         />
-        <div className="services_text">
-          <h2>Услуги по репродуктивному здоровью семьи</h2>
-          <p>
-            Делаем <span className="bold-text">быстрые обследования</span> для
-            <span className="bold-text"> женщин, мужчин и детей, </span>
-            малоинвазивные процедуры, эндокринологические и ультразвуковые
-            исследования, скриннинги -
-            <span className="bold-text"> весь спектр услуг</span> для здоровья и
-            хорошего самочувствия.
-          </p>
-          <img className="ellipse-image-left" src={left_button} alt="ellipse" />
-
-          <img
-            className="ellipse-image-right"
-            src={right_button}
-            alt="ellipse"
-          />
+        <div className="dots-buttons-container">
+          <Dots className="active-dot" />
+          <Dots className="inactive-dot color-gray margin-12" />
+          <Dots className="inactive-dot color-gray" />
+        </div>
+      </div>
+      <div className="services_text">
+        <h2>Услуги по репродуктивному здоровью семьи</h2>
+        <p>
+          Делаем <span className="bold-text">быстрые обследования</span> для
+          <span className="bold-text"> женщин, мужчин и детей, </span>
+          малоинвазивные процедуры, эндокринологические и ультразвуковые
+          исследования, скриннинги -
+          <span className="bold-text"> весь спектр услуг</span> для здоровья и
+          хорошего самочувствия.
+        </p>
+        <div className="arrow-button-container">
+          <LeftArrow className="services-margin-button-arrow" />
+          <RightArrow />
         </div>
       </div>
     </div>
